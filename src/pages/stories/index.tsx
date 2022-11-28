62919b0273ae5711f01d263e5e8c1b0dfe5c2c57
Joe.C.LEUNG <joe.c.leung@accenture.com>
2022-11-28 18:38:05 +0800

    First commit

---
+++ src/pages/stories/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import {HeadFC, PageProps} from "gatsby"
+
+const StoriesIndexPage: React.FC<PageProps> = () => {
+	return(
+		<main>
+			<h1>Stories</h1>
+			<ul>
+				<li>
+					<a href={`./greedyBear`}>
+						The Greedy Bear
+					</a>
+				</li>
+			</ul>
+		</main>
+	)
+}
+
+export default StoriesIndexPage
+
+export const Head: HeadFC = () => <title>Stories</title>
